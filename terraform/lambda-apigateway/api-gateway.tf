fc454f765141cc0351c6e78db61ab77d510702de
Giancarlo Petrini <giancarlo@libreworx.com>
2018-03-14 15:27:41 -0400

    add s3 terraform and travis env

--- terraform/lambda-apigateway/api-gateway.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_api_gateway_rest_api" "example" {
-  name        = "ServerlessExample"
-  description = "Terraform Serverless Application Example"
-}
--- terraform/lambda-apigateway/main.tf
+++ terraform/lambda-apigateway/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-resource "aws_s3_bucket" "lambda-apigateway" {
+resource "aws_s3_bucket" "lambda-apigateway-s3ls" {
   bucket        = "giancarlopetrini-s3-lambda"
   force_destroy = true
 
