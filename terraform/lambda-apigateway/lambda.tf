3f063fc181ec4e7fc946a3d722a180ae0eace508
Giancarlo Petrini <giancarlo@libreworx.com>
2018-03-13 20:59:36 -0400

    began building s3 in terraform main

--- terraform/lambda-apigateway/lambda.tf
+++ terraform/lambda-apigateway/lambda.tf
@@ -7,7 +7,7 @@ resource "aws_lambda_function" "example" {
 
   # The bucket name as created earlier with "aws s3api create-bucket"
   s3_bucket = "terraform-serverless-example"
-  s3_key    = "v1.0.0/example.zip"
+  s3_key    = "lambda-build/lambda.zip"
 
   # "main" is the filename within the zip file (main.js) and "handler"
   # is the name of the property under which the handler function was
@@ -39,17 +39,4 @@ resource "aws_iam_role" "lambda_exec" {
   ]
 }
 EOF
-}
-
-resource "aws_api_gateway_resource" "proxy" {
-  rest_api_id = "${aws_api_gateway_rest_api.example.id}"
-  parent_id   = "${aws_api_gateway_rest_api.example.root_resource_id}"
-  path_part   = "{proxy+}"
-}
-
-resource "aws_api_gateway_method" "proxy" {
-  rest_api_id   = "${aws_api_gateway_rest_api.example.id}"
-  resource_id   = "${aws_api_gateway_resource.proxy.id}"
-  http_method   = "ANY"
-  authorization = "NONE"
 }
---
+++ terraform/lambda-apigateway/main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  profile = "tfbuild"
+  region  = "us-east-1"
+}
+
+resource "aws_s3_bucket" "lambda-apigateway" {
+  bucket        = "giancarlopetrini-s3-lambda"
+  force_destroy = true
+
+  tags {
+    Name = "giancarlopetrini-s3-lambda-test"
+  }
+}
