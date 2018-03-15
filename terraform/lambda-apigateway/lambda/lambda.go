aa654e10355ca9f3947ad5d8aba8a1e0c5ed8140
Giancarlo Petrini <giancarlo@libreworx.com>
2018-03-14 22:14:51 -0400

    update lambda.go with proper lambda

--- terraform/lambda-apigateway/lambda/lambda.go
+++ terraform/lambda-apigateway/lambda/lambda.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"context"
 	"fmt"
+
+	"github.com/aws/aws-lambda-go/lambda"
 )
 
+// MyEvent - Name with json tag
+type MyEvent struct {
+	Name string `json:"name"`
+}
+
+// HandleRequest - takes json and returns name
+func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
+	return fmt.Sprintf("Hello %s!", name.Name), nil
+}
+
 func main() {
-	fmt.Println("Hello World")
+	lambda.Start(HandleRequest)
 }
