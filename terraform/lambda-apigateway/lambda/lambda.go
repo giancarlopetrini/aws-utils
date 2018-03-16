2e7277b5b09985ae4c5c39961708f4c91acb3891
Giancarlo Petrini <giancarlo@libreworx.com>
2018-03-16 14:00:46 -0400

    updated lambda.go to reply json format

--- terraform/lambda-apigateway/lambda/lambda.go
+++ terraform/lambda-apigateway/lambda/lambda.go
@@ -1,22 +1,28 @@
 package main
 
 import (
-	"context"
 	"fmt"
 
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-// MyEvent - Name with json tag
-type MyEvent struct {
+// Request - lambda expects json with name key
+type Request struct {
 	Name string `json:"name"`
 }
 
-// HandleRequest - takes json and returns name
-func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
-	return fmt.Sprintf("Hello %s!", name.Name), nil
+// Response - send back message
+type Response struct {
+	Message string `json:"message"`
+}
+
+// Handler - get name request, format and return response
+func Handler(request Request) (Response, error) {
+	return Response{
+		Message: fmt.Sprintf("Hello, %s!", request.Name),
+	}, nil
 }
 
 func main() {
-	lambda.Start(HandleRequest)
+	lambda.Start(Handler)
 }
