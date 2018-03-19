297de386a26256ab8013b356e3a84179f0d52805
Giancarlo Petrini <giancarlo@libreworx.com>
2018-03-19 18:35:11 -0400

    add subdomain record to main.tf

--- terraform/lambda-apigateway/main.tf
+++ terraform/lambda-apigateway/main.tf
@@ -11,3 +11,15 @@ resource "aws_s3_bucket" "lambda-apigateway-s3" {
     Name = "giancarlopetrini-s3-lambda-test"
   }
 }
+
+data "aws_route53_zone" "selected" {
+  name = "giancarlopetrini.com."
+}
+
+resource "aws_route53_record" "apitest" {
+  zone_id = "${data.aws_route53_zone.selected.zone_id}"
+  name    = "apitest.${data.aws_route53_zone.selected.name}"
+  type    = "CNAME"
+  ttl     = "3600"
+  records = ["${aws_api_gateway_deployment.lambda-gateway.invoke_url}"]
+}
